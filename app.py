19fc93c0f6520b3b660595c0021c6a3cec6f65c2
ohjho <jho.yvr+git@gmail.com>
2020-03-26 10:44:55 +0800

    added new utils function image_hstack

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-from utils import GetInputImage
+from utils import GetInputImage, image_hstack
 
 from ISR.models import RDN, RRDN
 
@@ -41,23 +41,25 @@ def Main():
     l_model_name = ['gans', 'psnr-large', 'psnr-small', 'noise-cancel']
     model_name = st.sidebar.selectbox('model type', options = l_model_name)
     st.sidebar.markdown('[-> details](https://github.com/idealo/image-super-resolution#pre-trained-networks)')
+    left_pct = st.sidebar.slider('Percentage of Orginal Image to show:', min_value = 0, max_value = 1.0, step = 0.1, value = 0.5)
 
     if type(img_np_arr) == np.ndarray:
         sr_img = sr_image(img_np_arr, model_name = model_name)
 
         print(f'org img shape: {img_np_arr.shape}\nSR img shape: {sr_img.shape}')
 
-        h, w, c = sr_img.shape
-        org_img = Image.fromarray(img_np_arr).resize(size = (w, h))
-        new_img = Image.fromarray(sr_img)
-        black_line_width = 5
-        np_black_line = np.zeros(shape = [h,black_line_width, c], dtype = np.uint8)
-        img_comb = np.hstack([
-                            np.asarray(org_img)[:, :int(w/2), :],
-                            np_black_line,
-                            np.asarray(new_img)[:, int(w/2):, :]
-                            ])
+        # h, w, c = sr_img.shape
+        # org_img = Image.fromarray(img_np_arr).resize(size = (w, h))
+        # new_img = Image.fromarray(sr_img)
+        # black_line_width = 5
+        # np_black_line = np.zeros(shape = [h,black_line_width, c], dtype = np.uint8)
+        # img_comb = np.hstack([
+        #                     np.asarray(org_img)[:, :int(w/2), :],
+        #                     np_black_line,
+        #                     np.asarray(new_img)[:, int(w/2):, :]
+        #                     ])
         st.subheader('Original vs Super Res')
+        img_comb = image_hstack(img_np_arr, sr_img, left_pct = left_pct, black_line_width = 5, up_scale = True)
         st.image(img_comb, channels = color_channels, use_column_width = True)
 
         # st.subheader('Original')
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import imageio
+from PIL import Image
 
 def GetInputImage(st_asset = st, type = ['jpg', 'png', 'jpeg'], color = 'RGB'):
 	'''
@@ -22,3 +23,33 @@ def GetInputImage(st_asset = st, type = ['jpg', 'png', 'jpeg'], color = 'RGB'):
 		return img
 	else:
 		return None
+
+def image_hstack(left_img_arr, right_img_arr, left_pct = None, black_line_width = 5, up_scale = False):
+	'''
+	return one image of left_img_arr and right_img_arr join together
+	Args:
+		left_img_arr: must have same aspect ratio as right_img_arr
+		left_pct: Percentage of left image to show, right image will fill up the remainder. If none, both left and right images will be shown in full
+		up_scale: if left and right images are different in size, use the bigger w & h
+	'''
+
+	h, w, __ = left_img_arr.shape
+	_h, _w, __ = right_img_arr.shape
+	if up_scale:
+		h, w = max(h, _h), max(w, _w)
+	else:
+		h, w = min(h, _h), min(w, _w)
+
+	left_pct = left_pct if left_pct else 1
+	if left_pct > 1:
+		raise TypeError(f"left_pct must be float and less than or equal to 1.")
+
+	l_img = Image.fromarray(left_img_arr).resize(size = (w,h))
+	r_img = Image.fromarray(right_img_arr).resize(size = (w,h))
+	np_black_line = np.zeros(shape = [h, black_line_width, 3], dtype = np.uint8)
+	img_comb = np.hstack([
+						np.asarray(l_img)[:, :(w * left_pct ), :],
+						np_black_line,
+						np.asarray(r_img)[:,(w * (1-left_pct)):,:]
+						])
+	return img_comb
